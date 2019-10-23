d9048669f5720bf3d7406778d151ba25e84ae3c6
npaulat <34585535+npaulat@users.noreply.github.com>
2019-10-23 12:34:06 -0500

    Update 23-OCT-2019_SGE-clusterrun.py

--- 23-OCT-2019_SGE-clusterrun.py
+++ 23-OCT-2019_SGE-clusterrun.py
@@ -14,6 +14,7 @@ import errno
 from Bio import SeqIO
 from Bio import SeqRecord
 from Bio import Seq
+from Bio.SeqRecord import SeqRecord
 #import numpy as np
 #import pandas as pd
 
@@ -302,20 +303,11 @@ def simple_partition():
 		print("  - {} batches with {} bp and one trailing batch with {} bp.".format(str(len(PARTS) - 1), str(CHUNK_SIZE), str(TRAILING_SIZE)))
 	else:
 		print("  - All batches contain exactly {} bp.".format(str(CHUNK_SIZE)))
-	
-
-##############################################################################
-############  EVERYTHING ABOVE THIS LINE IS FROM NIKKI'S SCRIPT    ##########
-############  IT IS NOT PROPERLY CONNECTED TO THE SCRIPT BELOW      ##########
-##############################################################################
 
 ### TO MERGE NIKKI AND MY SCRIPTS TOGETHER I CHANGED LINES 321 & 324
 ### LINE 321: USED THE "BATCH_COUNT" ARGUMENT FROM ARGPARSE
 ### LINE 324: USED THE "GENOME" ARGUMENT FROM ARGPARSE INSTEAD OF HARD CODING IN THE FILE
 
-
-from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
 
 # Set chunk number
 batch_number = BATCH_COUNT
@@ -329,75 +321,87 @@ chunk_size = sum(len(i) for i in records) // batch_number + 1
 
 
 # Start creating batches
-def create_batch(records, chunk_size):
+#def create_batch(records, chunk_size):
+def create_batch(BATCH_COUNT, GENOME):
+	
+	# Set chunk number
+	batch_number = BATCH_COUNT
+
+	# Create a list of all the seq records inside of the genome
+	records = list(SeqIO.parse(GENOME, "fasta"))
+
+	# Define the chunk size
+	# Sum of all lengths of all records diveded by "batch_number"
+	chunk_size = sum(len(i) for i in records) // batch_number + 1
     
 	# Create an object out of each record and go through them iteratively
-    record_it = iter(records)
+    	record_it = iter(records)
 
-    # The "next" object in the list of "records"... 
-    # Basically going through each contig one at a time
-    record = next(record_it)
+	# The "next" object in the list of "records"... 
+	# Basically going through each contig one at a time
+	record = next(record_it)
     
-    # Initiallize base pair counting
-    current_base = 0
+	# Initiallize base pair counting
+	current_base = 0
 
-    # Create a dictionary for batches and initialize the batch size
-    # "Batch" is defined as the output fasta file that has a collection of "chunks"
-    batch = []
-    batch_size = 0
+	# Create a dictionary for batches and initialize the batch size
+	# "Batch" is defined as the output fasta file that has a collection of "chunks"
+	batch = []
+	batch_size = 0
 
-    # While there are still records left in the list, keep creating new batches
-    while record:
+	# While there are still records left in the list, keep creating new batches
+	while record:
 
-        # Loop over records untill the batch is full (i.e. reached the max chunk size), or there are no new records 
-        while batch_size != chunk_size and record:
+		# Loop over records untill the batch is full (i.e. reached the max chunk size), or there are no new records 
+		while batch_size != chunk_size and record:
 
-        	# Define the end... which sums up to the chunk size
-            end = current_base + chunk_size - batch_size
+		# Define the end... which sums up to the chunk size
+		end = current_base + chunk_size - batch_size
 
-            # Define the output sequence, which is the current base (beginning base), seperated by a ":" and the end base of the contig
-            seq = record[current_base:end]
+		# Define the output sequence, which is the current base (beginning base), seperated by a ":" and the end base of the contig
+		seq = record[current_base:end]
 
-            # Define where to cut the contig off, which is the current base + the length of the output sequence defined above
-            end_of_slice = current_base + len(seq) - 1
+		# Define where to cut the contig off, which is the current base + the length of the output sequence defined above
+		end_of_slice = current_base + len(seq) - 1
 
-            # Create the fasta headers to match that of the original SGE script
-            # <original_contig_name> ":" <beginning_base> "-" <end_base>
-            fasta_header = record.id + ":{}-{}".format(current_base, end_of_slice)
+		# Create the fasta headers to match that of the original SGE script
+		# <original_contig_name> ":" <beginning_base> "-" <end_base>
+		fasta_header = record.id + ":{}-{}".format(current_base, end_of_slice)
 
-            # Change the seq.id to the fasta header defined above. 
-            seq.id = seq.name = fasta_header
+		# Change the seq.id to the fasta header defined above. 
+		#seq.id = seq.name = fasta_header
+		seq.id == fasta_header
 
-            # Set a blank description for the sequence.
-            # For some reason this throws off Biopython if there is nothing present in the description object. 
-            seq.description = ''
+		# Set a blank description for the sequence.
+		# For some reason this throws off Biopython if there is nothing present in the description object. 
+		#seq.description = ''
 
-            # Add the sequence to the current batch 
-            batch.append(seq)
+		# Add the sequence to the current batch 
+		batch.append(seq)
 
-            # This is where we start doing the math. 
-            # Add the lenth of the current sequence we are iterating through to the current base.
-            # When doing this, we also need to keep track of the batch_size... we want to make everything as equal as possible.
-            current_base += len(seq)
-            batch_size += len(seq)
+		# This is where we start doing the math. 
+		# Add the lenth of the current sequence we are iterating through to the current base.
+		# When doing this, we also need to keep track of the batch_size... we want to make everything as equal as possible.
+		current_base += len(seq)
+		batch_size += len(seq)
 
-            # When we have "added" all of the bases from the current sequence we are iterating through, 
-            # then we need to go and grab the next sequence in the list. 
-            if current_base >= len(record):
-                record = next(record_it, None)
-                current_base = 0
+		# When we have "added" all of the bases from the current sequence we are iterating through, 
+		# then we need to go and grab the next sequence in the list. 
+		if current_base >= len(record):
+			record = next(record_it, None)
+			current_base = 0
 
-        # Once we have a batch with the correct size, yield the batch.
-        # OR... we have run out of sequences in the genome, so stop. 
-        yield batch
-        batch = []
-        batch_size = 0
+		# Once we have a batch with the correct size, yield the batch.
+		# OR... we have run out of sequences in the genome, so stop. 
+		yield batch
+		batch = []
+		batch_size = 0
 
 # Write out the batches as new fasta files. 
 for i, batch in enumerate(create_batch(records, chunk_size)):
 
 	#Name the filed and keep track of the numbering. 
-    filename = "chunk{}.fasta".format(i)
+    filename = "{:03d}.fa".format(i)
 
     # Write all the batch'e's sequences and their appropriate headers to the output fasta file. 
     SeqIO.write(batch, filename, "fasta")
